db51a90d87216610c93bc9da0a782bb980c4cd12
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2023-03-04 13:03:10 +0300

    Değişkenler ve veri tipleri dersi girişi yapıldı
    - Tam sayılar
    - Ondalık sayılar
    - Metin
    veri tipleri öğrenildi

---
+++ 01_degiskenler_ve_veri_tipleri.py
@@ -0,0 +1,21 @@
+# Değişken tanımlama örnekleri
+x = 5
+y = "Merhaba, Dünya!"
+z = True
+
+# VERİ TİPLERİ
+# 1. Tam sayılar (integer)
+x = 15
+y = -15
+z = 0
+
+# 2. Ondalıklı sayılar (floats)
+x = 3.14
+y = -2.25
+z = 1.0
+
+# 3. Metin (string)
+selam = "Merhaba!"
+ad = "Eren"
+mesaj = selam + " " + ad
+print(mesaj)
