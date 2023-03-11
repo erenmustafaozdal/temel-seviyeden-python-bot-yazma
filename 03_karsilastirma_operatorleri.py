a49edb01a56774eeba752a3f692c2bb433ec3880
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2023-03-11 13:01:03 +0300

    Karşılaştırma operatörleri konusu girişi yapıldı

---
+++ 03_karsilastirma_operatorleri.py
@@ -0,0 +1,15 @@
+# Eşit mi?
+x = 5
+y = 10
+print(x == y)  # Çıktı: False
+print(x == 5)  # Çıktı: True
+
+# Eşit değil mi?
+print(x != y)  # Çıktı: True
+print(y != 10)  # Çıktı: False
+
+# Küçük mü?
+print(x < y)  # Çıktı: True
+print(y < x)  # Çıktı: False
+print("x + 6 < y :", x + 6 < y)  # Çıktı: False
+
