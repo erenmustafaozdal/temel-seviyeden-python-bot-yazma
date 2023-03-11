0bfd1e0373f009fd07f461272fde0ee6b8f2e18c
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2023-03-11 12:43:12 +0300

    Aritmetik operatörler konusu bitti

---
+++ 02_aritmetik_operatorler.py
@@ -0,0 +1,56 @@
+"""
+Toplam: +
+Çıkarma: -
+Çarpma: *
+Bölme: /
+Tam Bölme: //
+Mod Alma: %
+Üs Alma: **
+"""
+
+# Toplama işlemi
+a = 5
+b = 3
+c = a + b
+print(c)  # Çıktı: 8
+
+# Çıkarma işlemi
+d = 10
+e = 6
+f = d - e
+print(f)  # Çıktı: 4
+
+# Çarpma işlemi
+g = 6
+h = 8
+i = g * h
+print(i)  # Çıktı: 48
+
+# Bölme işlemi
+j = 86
+k = 7
+l = j / k
+print(l)  # Çıktı: 12.285714285714286
+
+# Tam Bölme işlemi
+m = j // k
+print(m)  # Çıktı: 12
+n = -86
+o = n // k
+print(o)  # Çıktı: -13
+
+# Mod Alma işlemi
+p = j % k
+print(p)  # Çıktı: 2
+r = n % k
+print(r)  # Çıktı: 5
+
+# Üs Alma işlemi
+s = 2
+t = 3
+u = s ** t
+print(u)  # Çıktı: 8
+
+# İŞLEM ÖNCELİĞİ
+print(5 + 4 ** 3)  # Çıktı: 69
+print((5 + 4) ** 3)  # Çıktı: 729
