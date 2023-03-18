7fadc88c11c51284238143407dadb5ec0f803d56
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2023-03-18 12:50:51 +0300

    Üyelik operatörleri konusu  bitti

---
+++ 06_uyelik_operatorleri.py
@@ -0,0 +1,11 @@
+# Üyelik operatörleri: in, not in
+
+liste = [1, 2, 3, 4, 5]
+
+# in Operatörü
+print(4 in liste)  # Çıktı: True
+print(6 in liste)  # Çıktı: False
+
+# not in Operatörü
+print(4 not in liste)  # Çıktı: False
+print(6 not in liste)  # Çıktı: True
