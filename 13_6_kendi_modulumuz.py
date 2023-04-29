6a644c6d5dc20fe10db8c4ba8193e391c84536b0
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2023-04-29 10:32:54 +0300

    Kendi modülümüzü oluşturma

---
+++ 13_6_kendi_modulumuz.py
@@ -0,0 +1,14 @@
+# İÇE AKTARMA 1. Yöntem
+# import matematik_islemleri
+#
+# print(matematik_islemleri.topla(2, 3))
+# print(matematik_islemleri.cikar(2, 3))
+# print(matematik_islemleri.carp(2, 3))
+# print(matematik_islemleri.bol(2, 3))
+
+# İÇE AKTARMA 2. Yöntem
+# Hangisi işimize yarayacaksa onu import edelim
+from matematik_islemleri import topla, cikar
+
+print(topla(2, 3))
+print(cikar(2, 3))
---
+++ matematik_islemleri.py
@@ -0,0 +1,14 @@
+def topla(a, b):
+    return a + b
+
+
+def cikar(a, b):
+    return a - b
+
+
+def carp(a, b):
+    return a * b
+
+
+def bol(a, b):
+    return a / b
