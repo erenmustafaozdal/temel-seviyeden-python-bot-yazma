32cc76339e9a604c3c7c3a038324844fbbe60c61
Eren Mustafa ÖZDAL <eren.060737@gmail.com>
2024-02-03 22:18:51 +0300

    proje ana dosyası

--- 22_proje_hepsiemlak.py
+++ 22_proje_hepsiemlak.py
@@ -1,15 +1,35 @@
-import time
-import logging
+import traceback
 from modules.log import Log
 from modules.hepsiemlak import HepsiEmlak
+from modules.excel import Excel
+from db.model.listings import Listing
 
 
 # log ayarlarını yapalım
 Log("hepsiemlak")
-logger = logging.getLogger(__name__)
+
+# Excel işlemleri için nesneleri oluşturalım
+excel = Excel("./db/hepsiemlak.xlsx")
+# Çalışma sayfasını oluşturalım
+listing_sheet = Listing(excel.wb)
 
 # HepsiEmlak nesnesi oluştur ve arama yap
-s = HepsiEmlak()
-s.search(categories="Konut,Satılık,Daire")
+he = HepsiEmlak()
+he.set_location("Osmaniye")
+# he.set_category("Konut")
+# he.set_house_types(["Daire", "Villa"])
+# he.set_price(1000000, 2000000)
+# he.set_square_meters(150, 200)
+# he.set_room_types(["3+1", "4+1"])
+try:
+    results = he.search()
+except Exception as e:
+    he.driver.save_screenshot("./logs/error.png")
+    he.logger.error(
+        f"İlan arama sırasında hata oluştu: {traceback.format_exc()}")
+    results = []
 
-time.sleep(5)
+for result in results:
+    listing_sheet.insert(result.__dict__)
+
+excel.save()
